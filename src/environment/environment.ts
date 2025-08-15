53d54529e44b1a7ca573735988b94ca2d125c672
lucio.sacramento <lucio.sacramento@tcm.ba.gov.br>
2025-08-15 13:26:00 -0300

    corrigindo API_url

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -3,5 +3,5 @@ import {versao} from './version';
 export const environment = {
   production: false,
   versao:versao,
-  API_url: 'https://hmall.sitenossajanela.com.br/wp-json/lojas/v1/',
+  API_url: 'https://hmall.sitenossajanela.com.br/wp-json/lojas/v1/'
 };
